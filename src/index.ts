b9c8789adf46699e8abe984fe04452cfeb67ab57
Paulo Martins <paulo.martins@simbioseventures.com>
2022-11-21 09:02:31 -0300

    feat: add rate limiter

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import express, {Express} from 'express';
 import {userRouter} from './routes/userRoutes';
 import {loginRouter} from './routes/loginRoutes';
+import { rateLimiter } from './rateLimiter';
 
 const app: Express = express();
 app.use(express.json());
+app.use(rateLimiter);
 app.use("/users", userRouter);
 app.use("/login", loginRouter);
 
--- src/services/auth.ts
+++ src/services/auth.ts
@@ -4,24 +4,24 @@ import jwt from 'jsonwebtoken'
 const prisma = new PrismaClient();
 
 export function auth( id: number) {
-    const token = jwt.sign({ data: 'foobar', userId: id }, 'secret', { expiresIn: '1h' });
-    return token;
+  const token = jwt.sign({ data: 'foobar', userId: id }, 'secret', { expiresIn: '1h' });
+  return token;
 };
 
 export async function ensureAuthenticated(req: any, res: any, next: any) {
-    try {
-      const token = req.headers.authorization;
-      const decodedToken = jwt.verify(token, 'secret');
-      const userId = decodedToken.userId;
-      const confirmUser = await prisma.user.findUnique({ where: {id: userId}})
-      if (confirmUser.id !== userId) {
-        throw 'Invalid user';
-      } else {
-        return next();
-      }
-    } catch {
-      res.status(401).json({
-        error: new Error('Invalid request!')
-      });
+  try {
+    const token = req.headers.authorization;
+    const decodedToken = jwt.verify(token, 'secret');
+    const userId = decodedToken.userId;
+    const confirmUser = await prisma.user.findUnique({ where: {id: userId}})
+    if (confirmUser.id !== userId) {
+      throw 'Invalid user';
+    } else {
+      return next();
     }
+  } catch {
+    res.status(401).json({
+      error: new Error('Invalid request!')
+    });
+  }
 };
